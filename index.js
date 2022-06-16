1d290b128f6541f9287031687a0dc3452967bb43
Akram M. Harazem <67602156+AkramHarazem@users.noreply.github.com>
2022-06-16 11:02:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
     var buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+function makeSound (key){
+
+    switch (key) {
       case "w":
         var tom1 = new Audio("sounds/tom-1.mp3");
         tom1.play();
@@ -36,6 +57,11 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
       default:
     }
 
-  });
+  };
+
+function buttonAnimation (currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add ("pressed");
+  setTimeout (function () {activeButton.classList.remove ("pressed");}, 100);
 
 }
